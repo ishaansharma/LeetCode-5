0614f5a596ff065e721f9772ddb2b01e6725f02a
Jerry Jiang <37967938+jjiang0595@users.noreply.github.com>
2022-03-06 16:09:00 -0500

    Create TwoSum.py

---
+++ Python3/Easy/1 - Two Sum/TwoSum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for index in range(i + 1, len(nums)):
+                if target - nums[i] == nums[index]:
+                    return i, index
