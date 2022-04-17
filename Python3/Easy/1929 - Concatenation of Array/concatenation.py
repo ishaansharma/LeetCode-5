bd96f66429924a123a91c06199bd135d34a83d63
Jerry Jiang <37967938+jjiang0595@users.noreply.github.com>
2022-04-17 02:17:53 -0400

    Create concatenation.py

---
+++ Python3/Easy/1929 - Concatenation of Array/concatenation.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.extend(nums)
+        return nums
+
+    
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums + nums
+
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums * 2
+      
