98621ce9efe8c2a4286d735134941ebf33efd48a
Jerry Jiang <37967938+jjiang0595@users.noreply.github.com>
2022-04-18 16:18:47 -0400

    Update PreorderTraversal.py

--- Python3/Easy/144. Binary Tree Preorder Traversal/PreorderTraversal.py
+++ Python3/Easy/144. Binary Tree Preorder Traversal/PreorderTraversal.py
@@ -22,3 +22,20 @@ class Solution(object):
             res.append(root.val)
             self.helper(root.right, res)
             
+# ITERATIVE
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        self.helper(root, res)
+        return res
+    
+    def helper(self, root, res):
+        if root:
+            res.append(root.val)
+            self.helper(root.left, res)
+            self.helper(root.right, res)
+        
