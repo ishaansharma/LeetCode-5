0160e093bf364becbb818cbb6bf86ab06ccc2f76
Jerry Jiang <37967938+jjiang0595@users.noreply.github.com>
2022-04-18 15:42:37 -0400

    Update InorderTraversal.py

--- Python3/Easy/94. Binary Tree Inorder Traversal/InorderTraversal.py
+++ Python3/Easy/94. Binary Tree Inorder Traversal/InorderTraversal.py
@@ -25,13 +25,8 @@ class Solution(object):
                     stack.append((node.left, False))
         return res
       
+        
 # RECURSIVE
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def inorderTraversal(self, root):
         """
