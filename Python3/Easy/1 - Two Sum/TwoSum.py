4e766e4b7c30bc0cf1a347b24c37bf38c219c519
Jerry Jiang <37967938+jjiang0595@users.noreply.github.com>
2022-04-19 18:17:01 -0400

    Revisiting Two Sum

--- Python3/Easy/1 - Two Sum/TwoSum.py
+++ Python3/Easy/1 - Two Sum/TwoSum.py
@@ -1,6 +1,19 @@
+# O(n^2)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for index in range(i + 1, len(nums)):
                 if target - nums[i] == nums[index]:
                     return i, index
+                
+# O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        array = {}
+        
+        for index, value in enumerate(nums):
+            key = target - value
+            if key in array:
+                return [array[key], index]
+            else:
+                array[value] = index 
