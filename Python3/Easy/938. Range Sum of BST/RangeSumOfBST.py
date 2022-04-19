cc5a6124f10b736fb24f6d161f6e6d37e0b25294
Jerry Jiang <37967938+jjiang0595@users.noreply.github.com>
2022-04-18 22:39:39 -0400

    Update RangeSumOfBST.py

--- Python3/Easy/938. Range Sum of BST/RangeSumOfBST.py
+++ Python3/Easy/938. Range Sum of BST/RangeSumOfBST.py
@@ -24,3 +24,24 @@ class Solution:
                 if high < node.val and node.left:
                     stack.append(node.left)
         return res
+    
+# RECURSION
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        self.res = 0
+        self.rangeSum(root, low, high)
+        return self.res
+    
+    def rangeSum(self, root, low, high):
+        if root:
+            if low <= root.val <= high:
+                self.res += root.val
+                if root.left:
+                    self.rangeSum(root.left, low, high)
+                if root.right:
+                    self.rangeSum(root.right, low, high)
+            elif root.val < low and root.right:
+                self.rangeSum(root.right, low, high)
+            elif root.val > high and root.left:
+                self.rangeSum(root.left, low, high)
+    
