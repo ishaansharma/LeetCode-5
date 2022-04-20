8711a5eb71c13b110765102694eb2785c6bcc263
Jerry Jiang <37967938+jjiang0595@users.noreply.github.com>
2022-04-19 22:55:02 -0400

    Update MergeTwoSortedLists.py

--- Python3/Easy/21 - Merge Two Sorted Lists/MergeTwoSortedLists.py
+++ Python3/Easy/21 - Merge Two Sorted Lists/MergeTwoSortedLists.py
@@ -19,3 +19,16 @@ class Solution:
         curr.next = l1 or l2
         return dummy.next
         
+# RECURSIVE
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
