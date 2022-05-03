c0adee4c3585d77c75d518cdb9ddb9004fbc0c13
Jerry Jiang <jjiang0595@bths.edu>
2022-05-02 23:15:56 -0400

    Search a 2D Matrix

--- Python3/Medium/74 - Search a 2D Matrix/SearchA2DMatrix.py
+++ Python3/Medium/74 - Search a 2D Matrix/SearchA2DMatrix.py
@@ -18,3 +18,32 @@ class Solution:
             else:
                 r = middle - 1
         return False       
+
+# Second Attempt
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:   
+        top, bottom = 0, len(matrix) - 1
+        row = 0
+        
+        while top <= bottom:
+            row = top + ((bottom - top) // 2)
+            
+            if matrix[row][0] > target:
+                bottom = row - 1
+            elif matrix[row][-1] < target:
+                top = row + 1
+            else:
+                break
+            
+        l, r = 0, len(matrix[row]) - 1
+        
+        while l <= r:
+            middle = l + ((r - l) // 2)
+            
+            if matrix[row][middle] == target:
+                return True
+            elif matrix[row][middle] < target:
+                l = middle + 1
+            else:
+                r = middle - 1
+        return False       
