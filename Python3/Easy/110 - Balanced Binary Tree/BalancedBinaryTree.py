fa361a1099086231fb457a54cad77a89afb06a46
Jerry Jiang <jjiang0595@bths.edu>
2022-05-14 00:35:15 -0400

    Balanced Binary Tree

--- Python3/Easy/110 - Balanced Binary Tree/BalancedBinaryTree.py
+++ Python3/Easy/110 - Balanced Binary Tree/BalancedBinaryTree.py
@@ -20,3 +20,21 @@ class Solution:
         statement = True
         height(root)
         return statement
+
+# REVISED SOLUTION
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def height(node):
+            if not node:
+                return 0   
+            left = height(node.left)
+            if left == -1:
+                return -1
+            right = height(node.right)
+            if right == -1:
+                return -1
+            if abs(left - right) > 1:
+                return -1
+            return 1 + max(left, right)
+        return height(root) != -1
+    
