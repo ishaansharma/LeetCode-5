8f8a887049d7757b780f39e312efc3d1d22d91fc
Jerry Jiang <jjiang0595@bths.edu>
2022-05-15 22:05:26 -0400

    Binary Tree Order Level Order Traversal

--- Python3/Easy/102 - Binary Tree Level Order Traversal/BinaryTreeOrder.py
+++ Python3/Easy/102 - Binary Tree Level Order Traversal/BinaryTreeOrder.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# BFS
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         stack = deque([root])
@@ -21,4 +23,19 @@ class Solution:
             res.append(temp)
         return res
                 
-                
+# DFS
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        
+        def dfs(node, depth):
+            if not node:
+                return
+            if len(res) == depth:
+                res.append([])
+            res[depth].append(node.val)
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+        dfs(root, 0)
+        return res
+    
