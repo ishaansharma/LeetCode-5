299fa7ca25a14c2b70722a950051a7fb6582e82f
Jerry Jiang <jjiang0595@bths.edu>
2022-05-22 06:39:21 -0400

    K Closest Points to Origin

--- Python3/Medium/973. K Closest Points to Origin/KClosestPointsToOrigin.py
+++ Python3/Medium/973. K Closest Points to Origin/KClosestPointsToOrigin.py
@@ -13,4 +13,18 @@ class Solution:
             dist, x, y = heapq.heappop(values)
             res.append([x, y])
         return res
-      
+
+# MAX HEAP
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+        
+        for x, y in points:
+            dist = -(x**2 + y**2)
+            if len(heap) == k:
+                heapq.heappushpop(heap, [dist, x, y])
+            else:
+                heapq.heappush(heap, [dist, x, y])
+        
+        return [(x, y) for dist, x, y in heap]
+                
