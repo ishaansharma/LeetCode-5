2dd4d5e036c1fe2dd0d1bd9074e3ec9cb61025c9
Jerry Jiang <jjiang0595@bths.edu>
2022-05-22 15:34:20 -0400

    Task Scheduler

---
+++ Python3/Medium/621 - Task Scheduler/TaskScheduler.py
@@ -0,0 +1,20 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count = Counter(tasks)
+        heap = [-c for c in count.values()]
+        heapq.heapify(heap)
+        
+        time = 0
+        q = deque()
+        
+        while heap or q:
+            time += 1
+            if heap:
+                c = 1 + heapq.heappop(heap)
+                if c:
+                    q.append([c, time + n])
+            if q:
+                if q[0][1] == time:
+                    heapq.heappush(heap, q.popleft()[0])
+        return time
+      
