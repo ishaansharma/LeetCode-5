ff3c11ebc3e6a2c826d1573d689fb116a9df613f
Ishaan Sharma <officialishaansharma@gmail.com>
2022-12-15 10:35:46 +0000

    Update PalindromeNumber.py
    Implemented Palindrome Number check without converting the given integer to string.

--- Python3/Easy/9 - Palindrome Number/PalindromeNumber.py
+++ Python3/Easy/9 - Palindrome Number/PalindromeNumber.py
@@ -1,4 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
-      
+        # return str(x) == str(x)[::-1]
+        if x < 0:
+            return False
+        copy, reverse = x, 0
+
+        while copy:
+            reverse *= 10
+            reverse += copy % 10
+            copy //= 10
+
+        return x == reverse
